85e267a08abf1d2e5bf352cb80bc4a4a120043c6
LydiaKoerber <lydi.koerber@web.de>
2023-07-31 11:09:20 +0200

    fix first demo

--- demo/demo_quaxa.py
+++ demo/demo_quaxa.py
@@ -1,6 +1,7 @@
 import conllu
 import random
 import quaxa
+import quaxa.reader
 
 random.seed(42)
 
@@ -8,17 +9,17 @@ random.seed(42)
 def demo():
     # read conllu file
     corpus = conllu.parse(open('demo.conllu', 'r').read())
-    sents, annot = reader.parse_conllu(corpus)
+    sents, annot = quaxa.reader.parse_conllu(corpus)
     # compute scores for example sentences
     print(f'{sents[0]}')
     for sent, anno in zip(sents, annot):
-        lemmas = [tok.get('lemma') for tok in anno]
-        headword = random.choice(lemmas)
-        factor = quaxa.total_score(
+        lemmas_content = [tok.get('lemma') for tok in anno if tok.get('upos') in {'NOUN', 'VERB', 'ADJ'}]
+        for headword in lemmas_content:
+            factor = quaxa.total_score(
                         headword=headword, txt=sent, annotation=anno)
-        print((
+            print((
             "total_score:"
-            f"{factor: 7.4f}  | {headword} | {sent[:20]} ..."))
+            f"{factor: 7.4f}  | {headword} | {sent[:50]} ..."))
 
 
 if __name__ == '__main__':
