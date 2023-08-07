26d927b9c824a2d80bb579063bcaa165f5c50b6f
Ulf Hamster <554c46@gmail.com>
2023-08-07 07:55:46 +0200

    remove reader util fun

--- demo/demo_quaxa.py
+++ demo/demo_quaxa.py
@@ -9,17 +9,19 @@ random.seed(42)
 def demo():
     # read conllu file
     corpus = conllu.parse(open('demo.conllu', 'r').read())
-    sents, annot = quaxa.reader.parse_conllu(corpus)
     # compute scores for example sentences
-    print(f'{sents[0]}')
-    for sent, anno in zip(sents, annot):
-        lemmas_content = [tok.get('lemma') for tok in anno if tok.get('upos') in {'NOUN', 'VERB', 'ADJ'}]
+    for annot in corpus:
+        lemmas_content = [
+            tok.get('lemma') for tok in annot 
+            if tok.get('upos') in {'NOUN', 'VERB', 'ADJ'}
+        ]
+        sent = annot.metadata['text']
         for headword in lemmas_content:
             factor = quaxa.total_score(
-                        headword=headword, txt=sent, annotation=anno)
+                headword=headword, txt=sent, annotation=annot)
             print((
-            "total_score:"
-            f"{factor: 7.4f}  | {headword} | {sent[:50]} ..."))
+                "total_score:"
+                f"{factor: 7.4f}  | {headword} | {sent[:50]} ..."))
 
 
 if __name__ == '__main__':
