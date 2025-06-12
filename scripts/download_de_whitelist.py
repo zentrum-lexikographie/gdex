c2c1d956f446c21b40ef5352126dde0e46b212f3
Natalie-T-E <natalie.suermeli@gmail.com>
2025-06-12 15:12:22 +0200

    Use JSON (instead of CSV) lemma database to prepare for further modifications

--- scripts/download_de_whitelist.py
+++ scripts/download_de_whitelist.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 import csv
-import io
+import json
 import urllib.request
 from pathlib import Path
 
 project_dir = Path(__file__).parent.parent.resolve()
 de_vulger_file = project_dir / "gdex" / "VulGer.csv"
 de_vulger_blacklist = set()
+de_whitelist = set()
+
+with urllib.request.urlopen("https://www.dwds.de/lemma/json") as f:
+    lemma_database = json.load(f)
 
 with de_vulger_file.open(encoding="utf-8") as vulger:
     for n, (word, score) in enumerate(csv.reader(vulger)):
@@ -17,20 +21,17 @@ with de_vulger_file.open(encoding="utf-8") as vulger:
             continue
         de_vulger_blacklist.add(word)
 
-de_whitelist = set()
-with urllib.request.urlopen("https://www.dwds.de/lemma/csv") as f:
-    lemmata = f.read().decode("utf-8")
-    for lemma in csv.DictReader(io.StringIO(lemmata)):
-        freq_class = lemma["frequenzklasse"]
-        if freq_class == "n/a":
-            continue
-        freq_class = int(freq_class)
-        if freq_class < 2:
-            continue
-        lemma_form = lemma["lemma"]
-        if lemma_form in de_vulger_blacklist:
-            continue
-        de_whitelist.add(lemma_form)
+for lemma in lemma_database:
+    freq_class = lemma["freq"]
+    if freq_class == "n/a":
+        continue
+    freq_class = int(freq_class)
+    if freq_class < 2:
+        continue
+    lemma_form = lemma["lemma"]
+    if lemma_form in de_vulger_blacklist:
+        continue
+    de_whitelist.add(lemma_form)
 
 de_whitelist_file = project_dir / "gdex" / "de_whitelist.txt"
 de_whitelist_file.write_text("\n".join(sorted(de_whitelist)), encoding="utf-8")
